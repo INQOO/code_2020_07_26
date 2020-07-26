0f15b85c118159fe20439218259b314c0bbf8429
Olaf Goj <olaf@goj.email>
2020-07-26 09:30:35 +0200

    Add arrays examples

--- src/com/inqoo/arrays/Main.java
+++ src/com/inqoo/arrays/Main.java
@@ -1,10 +1,5 @@
 package com.inqoo.arrays;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +7,10 @@ public class Main {
                 {"PL", "pon", "wt", "sr", "czw", "pt", "sb", "nd"},
                 {"EN", "mon", "tue", "wed", "thu", "fri", "sat", "sun"}
         };
+        System.out.println("Dlugosc tablicy: " + weekDays.length);
+        System.out.println("Dlugosc pierwszego elementu tablicy weekdays: " + weekDays[0].length);
+        System.out.println("Niedziela po angielsku to: " + weekDays[1][7]);
+        // petla for ktora przechodzi po kazdym elemencie tablicy (ktory to jest kolejna tablica)
         for (int i = 0; i < weekDays.length; i++) {
             System.out.println("LANG: " + weekDays[i][0]);
 
--- src/com/inqoo/arrays/StringArray.java
+++ src/com/inqoo/arrays/StringArray.java
@@ -1,8 +1,16 @@
 package com.inqoo.arrays;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class StringArray {
     public static void main(String[] args) {
         // stworzyc tablice stringow menu o dlugosci 7
         // w petli do kazdego elementu przypisz Kasza
+
+        String[] menu = new String[7];
+
+        Arrays.fill(menu, "Kasza");
+        System.out.println(Arrays.toString(menu));
     }
 }
