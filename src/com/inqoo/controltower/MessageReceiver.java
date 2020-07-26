f2ca09a16667fb6b908654ad4194d5f62ff89eab
Olaf Goj <olaf@goj.email>
2020-07-26 14:34:56 +0200

    Add getName to MessageReceiver

--- src/com/inqoo/controltower/MessageReceiver.java
+++ src/com/inqoo/controltower/MessageReceiver.java
@@ -3,8 +3,8 @@ package com.inqoo.controltower;
 import java.util.List;
 
 interface MessageReceiver {
+    String getName();
     void saveMessage(String message, MessageLevel level);
-
     List<String> getMessages();
 
 }
