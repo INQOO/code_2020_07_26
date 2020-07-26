29afb6ea487796d8218a3440828973b2afe87b86
Olaf Goj <olaf@goj.email>
2020-07-26 09:29:57 +0200

    Add SortNumbers instructions

---
+++ src/com/inqoo/arrays/SortNumbers.java
@@ -0,0 +1,12 @@
+package com.inqoo.arrays;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+        // stworzyc tablice intow o nazwie numbers o dlugosci 10
+        // wypelnic ta tablice losowymi liczbami (Arrays.fill + stworzyc obiekt klasy Random i go uzyc)
+        // wypisujemy ta tablice (Arrays toString)
+        // sortujemy ta tablice (od najmniejszej do najwiekszej)
+        // wypisujemy Posortowano:
+        // wypisujemy posortowana tablice (Arrays toString)
+    }
+}
