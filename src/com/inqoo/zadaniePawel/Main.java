98e12d120bacb939ac2ea6d82f7c1236a78c5dc2
Renovatio89 <lachpawel89@gmail.com>
2020-08-02 16:52:20 +0200

    Zadanie domowe czesc 3 - zrobiona

--- src/com/inqoo/zadaniePawel/Main.java
+++ src/com/inqoo/zadaniePawel/Main.java
@@ -1,15 +1,13 @@
 package com.inqoo.zadaniePawel;
 
-import static com.inqoo.zadaniePawel.Methods.startAgeTeen;
-import static com.inqoo.zadaniePawel.Methods.startArray9;
+import static com.inqoo.zadaniePawel.Methods.*;
 
 public class Main {
     public static void main(String[] args) {
         startAgeTeen();
         System.out.println(" ");
-        startAgeTeen();
-        System.out.println(" ");
         startArray9();
-
+        System.out.println(" ");
+        startFirstChar();
     }
 }
--- src/com/inqoo/zadaniePawel/Methods.java
+++ src/com/inqoo/zadaniePawel/Methods.java
@@ -1,13 +1,11 @@
 package com.inqoo.zadaniePawel;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Methods {
-
     // Cwiczenie 1
     public static boolean loneTeen(int a, int b) {
         boolean aTeen = (a >= 13 && a <= 19);
@@ -58,7 +56,32 @@ public class Methods {
         System.out.println(Arrays.toString(newRandomArray));
         out.println("W wylosowanej tablicy składającej się z " + elementsNumbers + " elementów cyfra '9' znajduje się: " + arrayCount9(newRandomArray));
     }
+
     // Cwiczenie 3
-
-
+    public static void startFirstChar() {
+        out.println("Podaj liczbe wyrazów, które uporządkujemy według pierwszej litery");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfWords = scanner.nextInt();
+        out.println("Wypiszesz tyle wyrazów: " + numberOfWords + ", ale najpierw podaj pierwsze:");
+        List<String> words = new ArrayList(Arrays.asList());
+        for (int i = 0; i < numberOfWords; i++) {
+            Scanner scanner2 = new Scanner(System.in);
+            String word = scanner.next();
+            words.add(word);
+            if (i < numberOfWords - 1) {
+                out.println("Podaj kolejny wyraz");
+            }
+        }
+        Map<String, List<String>> map = new HashMap();
+        for (String word : words) {
+            String firstChar = String.valueOf(word.charAt(0));
+            if (map.get(firstChar) == null) {
+                map.put(firstChar, new ArrayList(Arrays.asList(word)));
+            } else {
+                map.get(firstChar).add(word);
+            }
+        }
+        out.println("Nasze słowa to: " + words);
+        out.println("Uporządkowane słowa to: " + map);
+    }
 }
